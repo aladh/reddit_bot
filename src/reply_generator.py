57e2147d5b3369d11e9d6576c169dbcf94314330
Alishan Ladhani <alishan.ladhani@gmail.com>
2018-07-21 21:26:12 -0400

    Inject random selector

--- src/reply_generator.py
+++ src/reply_generator.py
@@ -1,11 +1,9 @@
-import random
-
-
 class ReplyGenerator:
-    def __init__(self, quotes):
-        random.seed()
+    def __init__(self, random, quotes):
+        self.random = random
         self.quotes = quotes
+        random.seed()
 
     def random_reply(self, comment):
-        quote = random.choice(self.quotes)
+        quote = self.random.choice(self.quotes)
         return quote(comment) if callable(quote) else quote
--- tests/reply_generator_test.py
+++ tests/reply_generator_test.py
@@ -1,9 +1,24 @@
 from src.reply_generator import ReplyGenerator
 
 quotes = [
-    'hello'
+    'hello',
+    'hi',
+    'bye'
 ]
 
 
-def test():
-    assert ReplyGenerator(quotes).random_reply(None) == quotes[0]
+class Random:
+    def __init__(self, choice_index):
+        self.choice_index = choice_index
+
+    # noinspection PyMethodMayBeStatic
+    def seed(self):
+        return None
+
+    def choice(self, array):
+        return array[self.choice_index]
+
+
+def test_selects_string_quote_using_injected_random():
+    reply = ReplyGenerator(Random(0), quotes).random_reply(None)
+    assert reply == quotes[0]
