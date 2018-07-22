0e54a3dddfa512749ae6f98900cf6e3e1fdd0814
Alishan Ladhani <alishan.ladhani@gmail.com>
2018-07-22 02:02:21 -0400

    Use word filter to block trigger words so the bot doesn't trigger itself

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import random
 from datetime import datetime
 from reply_chooser import ReplyChooser
 from lambda_quotes import LambdaQuotes
+from word_filter import WordFilter
 
 with open(f'{os.path.dirname(__file__)}/quotes.yml') as f:
     yaml_string_quotes = f.read()
@@ -19,6 +20,7 @@ subreddit = 'freebots'
 trigger = 'tyrion'
 
 reply_chooser = ReplyChooser(random, quotes)
+word_filter = WordFilter([trigger])
 
 print('Listening for triggers...')
 
@@ -26,6 +28,10 @@ for comment in reddit.subreddit(subreddit).stream.comments(skip_existing=True):
     try:
         if trigger in comment.body.lower():
             reply = reply_chooser.reply(comment)
+
+            if word_filter.is_blocked(reply):
+                print(f'{datetime.utcnow()} - WordFilter blocked reply: {reply}')
+                continue
 
             print(f'{datetime.utcnow()} - u/{comment.author.name} says: "{comment.body}". Replying with: "{reply}"')
 
