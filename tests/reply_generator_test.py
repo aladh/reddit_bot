9dc81b26314023bde42082d5a82d7ee63db90d49
Alishan Ladhani <alishan.ladhani@gmail.com>
2018-07-21 21:40:47 -0400

    Test lambda quote

--- tests/reply_generator_test.py
+++ tests/reply_generator_test.py
@@ -3,7 +3,9 @@ from src.reply_generator import ReplyChooser
 quotes = [
     'hello',
     'hi',
-    'bye'
+    'bye',
+    lambda comment: f'Hi, {comment.author.name}',
+    lambda comment: f'You said {comment.body.split()[0]}'
 ]
 
 
@@ -19,8 +21,29 @@ class Random:
         return array[self.choice_index]
 
 
+class Comment:
+    def __init__(self):
+        self.author = Author()
+        self.body = 'This is a comment'
+
+
+class Author:
+    def __init__(self):
+        self.name = 'foo'
+
+
 def test_selects_quote_using_injected_random():
     reply1 = ReplyChooser(Random(0), quotes).reply(None)
     reply2 = ReplyChooser(Random(1), quotes).reply(None)
     assert reply1 == quotes[0]
     assert reply2 == quotes[1]
+
+
+def test_handles_lambda_quote_with_author():
+    reply = ReplyChooser(Random(3), quotes).reply(Comment())
+    assert reply == 'Hi, foo'
+
+
+def test_handles_lambda_quote_with_body():
+    reply = ReplyChooser(Random(4), quotes).reply(Comment())
+    assert reply == 'You said This'
