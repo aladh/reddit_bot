8983dad335e9c1e8cb06526f71bf21c647113166
Alishan Ladhani <alishan.ladhani@gmail.com>
2018-07-22 01:15:05 -0400

    Listen to comments and reply when the trigger is received

--- src/main.py
+++ src/main.py
@@ -1,7 +1,35 @@
 import yaml
 import os
+import praw
+import random
+from datetime import datetime
+# noinspection PyUnresolvedReferences
+from reply_chooser import ReplyChooser
 
 with open(f'{os.path.dirname(__file__)}/quotes.yml') as f:
     yaml_string = f.read()
 
 string_quotes = yaml.load(yaml_string)
+
+quotes = string_quotes
+
+reddit = praw.Reddit('tyrion')
+
+subreddit = 'freebots'
+trigger = 'tyrion'
+
+reply_chooser = ReplyChooser(random, quotes)
+
+print('Listening for triggers...')
+
+for comment in reddit.subreddit(subreddit).stream.comments(skip_existing=True):
+    try:
+        if trigger in comment.body.lower():
+            reply = reply_chooser.reply(comment)
+
+            print(f'{datetime.utcnow()} - u/{comment.author.name} says: "{comment.body}". Replying with: "{reply}"')
+
+            comment.reply(reply)
+    except Exception as e:
+        print(f'{datetime.utcnow()} - Exception: ', e)
+        continue
