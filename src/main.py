57a4f762e106410bc3145df674eae4a48d7463e8
Alishan Ladhani <alishan.ladhani@gmail.com>
2018-07-22 23:30:49 -0400

    Support multiple triggers

--- src/main.py
+++ src/main.py
@@ -16,25 +16,29 @@ quotes = string_quotes + LambdaQuotes.all()
 reddit = praw.Reddit('tyrion')
 
 subreddit = 'freebots'
-trigger = 'tyrion'
+triggers = ['imp', 'dwarf', 'halfman', 'half-man', 'half man', 'monster', 'demon', 'monkey']
 
 reply_chooser = ReplyChooser(random, quotes)
-word_filter = WordFilter([trigger])
+word_filter = WordFilter(triggers)
 
 print('Listening for triggers...')
 
 for comment in reddit.subreddit(subreddit).stream.comments(skip_existing=True):
     try:
-        if trigger in comment.body.lower():
-            print(f'{datetime.utcnow()} - u/{comment.author.name} says: "{comment.body}"')
-            reply = reply_chooser.reply(comment)
+        for trigger in triggers:
+            if trigger in comment.body.lower():
+                print(f'{datetime.utcnow()} - Triggered by: "{trigger}"')
+                print(f'{datetime.utcnow()} - u/{comment.author.name} says: "{comment.body}"')
 
-            if word_filter.is_blocked(reply):
-                print(f'{datetime.utcnow()} - WordFilter blocked reply: {reply}')
-                continue
+                reply = reply_chooser.reply(comment)
 
-            print(f'{datetime.utcnow()} - Replying with: "{reply}"')
-            comment.reply(reply)
+                if word_filter.is_blocked(reply):
+                    print(f'{datetime.utcnow()} - WordFilter blocked reply: {reply}')
+                    continue
+
+                print(f'{datetime.utcnow()} - Replying with: "{reply}"')
+                print()
+                comment.reply(reply)
     except Exception as e:
         print(f'{datetime.utcnow()} - Exception: ', e)
         continue
