cf98a20c3ecf73b388a1218fb2ee69b7d6f5e983
Alishan Ladhani <alishan.ladhani@gmail.com>
2018-07-23 20:29:42 -0400

    Use trigger recognizer

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from reply_chooser import ReplyChooser
 from lambda_quotes import LambdaQuotes
 from word_filter import WordFilter
+from trigger_recognizer import TriggerRecognizer
 
 with open(f'{os.path.dirname(__file__)}/quotes.txt') as f:
     quotes_file = f.read()
@@ -20,27 +21,30 @@ triggers = ['imp', 'dwarf', 'halfman', 'half-man', 'half man', 'monster', 'demon
 
 reply_chooser = ReplyChooser(random, quotes)
 word_filter = WordFilter(triggers)
+trigger_recognizer = TriggerRecognizer(triggers)
 
 print('Listening for triggers...')
 
 for comment in reddit.subreddit(subreddit).stream.comments(skip_existing=True):
     try:
-        for trigger in triggers:
-            if trigger in comment.body.lower():
-                print(f'{datetime.utcnow()} - Triggered by: "{trigger}"')
-                print(f'{datetime.utcnow()} - u/{comment.author.name} says: "{comment.body}"')
+        trigger = trigger_recognizer.has_trigger(comment.body)
 
-                reply = reply_chooser.reply(comment)
+        if not trigger:
+            continue
 
-                if word_filter.is_blocked(reply):
-                    print(f'{datetime.utcnow()} - WordFilter blocked reply: {reply}')
-                    continue
+        print(f'{datetime.utcnow()} - Triggered by: "{trigger}"')
+        print(f'{datetime.utcnow()} - u/{comment.author.name} says: "{comment.body}"')
 
-                print(f'{datetime.utcnow()} - Replying with: "{reply}"')
-                print()
+        reply = reply_chooser.reply(comment)
 
-                comment.reply(reply)
-                break  # TODO: test this
+        if word_filter.is_blocked(reply):
+            print(f'{datetime.utcnow()} - WordFilter blocked reply: {reply}')
+            continue
+
+        print(f'{datetime.utcnow()} - Replying with: "{reply}"')
+        print()
+
+        comment.reply(reply)
     except Exception as e:
         print(f'{datetime.utcnow()} - Exception: ', e)
         continue
