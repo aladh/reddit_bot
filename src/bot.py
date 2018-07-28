54eff096564b97a48ffb1a59a0d0c03346939887
Alishan Ladhani <alishan.ladhani@gmail.com>
2018-07-28 14:45:58 -0400

    Extract bot class

---
+++ src/bot.py
@@ -0,0 +1,69 @@
+import random
+from datetime import datetime
+import praw
+# noinspection PyUnresolvedReferences
+from util.word_matcher import WordMatcher
+# noinspection PyUnresolvedReferences
+from util.reply_chooser import ReplyChooser
+
+
+class ReplierConfig:
+    def __init__(self, triggers, replies, test):
+        self.triggers = triggers  # TODO: Allow regex
+        self.replies = replies
+        self.test = test
+
+
+class ListenerConfig:
+    def __init__(self, subreddit):
+        self.subreddit = subreddit
+
+
+class Bot:
+    _listener = None
+    _repliers = []
+
+    def __init__(self, account):
+        self.account = account
+
+    def listen(self, subreddit):
+        self._listener = ListenerConfig(subreddit)
+        return self
+
+    def reply(self, triggers, replies, test=False):
+        self._repliers.append(ReplierConfig(triggers, replies, test))
+        return self
+
+    def run(self):
+        print('Listening for triggers...')
+        reddit = praw.Reddit(self.account)
+
+        for comment in reddit.subreddit(self._listener.subreddit).stream.comments(skip_existing=True):
+            for replier in self._repliers:
+                word_matcher = WordMatcher(replier.triggers)
+                reply_chooser = ReplyChooser(random, replier.replies)
+
+                try:
+                    trigger = word_matcher.is_present(comment.body)
+
+                    if not trigger:
+                        continue
+
+                    if comment.author == reddit.user.me():
+                        continue
+
+                    print(f'{datetime.utcnow()} {"TEST" if replier.test else ""}- Triggered by: "{trigger}"')
+                    print(f'{datetime.utcnow()} - u/{comment.author.name} says: "{comment.body}"')
+
+                    reply = reply_chooser.reply(comment)
+
+                    print(f'{datetime.utcnow()} - Replying with: "{reply}"')
+                    print()
+
+                    if replier.test:
+                        continue
+
+                    comment.reply(reply)
+                except Exception as e:
+                    print(f'{datetime.utcnow()} - Exception: ', e)
+                    continue
--- src/main.py
+++ src/main.py
@@ -1,50 +1,15 @@
 import os
-import praw
-import random
-from datetime import datetime
 # noinspection PyUnresolvedReferences
 from quotes.lambda_quotes import LambdaQuotes
+
 # noinspection PyUnresolvedReferences
-from util.reply_chooser import ReplyChooser
-# noinspection PyUnresolvedReferences
-from util.word_matcher import WordMatcher
+from bot import Bot
 
 with open(f'{os.path.dirname(__file__)}/quotes/quotes.txt') as f:
-    quotes_file = f.read()
+    quotes = f.read()
 
-string_quotes = quotes_file.split(sep='\n')
-
-quotes = string_quotes + LambdaQuotes.all()
-
-reddit = praw.Reddit('tyrion')
-
-subreddit = 'freebots'
-triggers = ['imp', 'dwarf', 'halfman', 'half-man', 'half man', 'tyrion bot', 'tyrion-bot']
-
-reply_chooser = ReplyChooser(random, quotes)
-word_matcher = WordMatcher(triggers)
-
-print('Listening for triggers...')
-
-for comment in reddit.subreddit(subreddit).stream.comments(skip_existing=True):
-    try:
-        trigger = word_matcher.is_present(comment.body)
-
-        if not trigger:
-            continue
-
-        if comment.author == reddit.user.me():
-            continue
-
-        print(f'{datetime.utcnow()} - Triggered by: "{trigger}"')
-        print(f'{datetime.utcnow()} - u/{comment.author.name} says: "{comment.body}"')
-
-        reply = reply_chooser.reply(comment)
-
-        print(f'{datetime.utcnow()} - Replying with: "{reply}"')
-        print()
-
-        comment.reply(reply)
-    except Exception as e:
-        print(f'{datetime.utcnow()} - Exception: ', e)
-        continue
+Bot(account='tyrion') \
+    .listen(subreddit='freebots') \
+    .reply(triggers=['imp', 'dwarf', 'halfman', 'half-man', 'half man', 'tyrion bot', 'tyrion-bot'],
+           replies=quotes.split(sep='\n') + LambdaQuotes.all()) \
+    .run()
