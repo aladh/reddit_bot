09d1077742ea0964061ac6672ac451aac8db96e7
Alishan Ladhani <alishan.ladhani@gmail.com>
2018-10-08 21:34:45 -0400

    Use environment variable to get subreddit name

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ with open(f'{os.path.dirname(__file__)}/quotes/quotes.txt') as f:
     quotes = f.read()
 
 Bot(account='tyrion') \
-    .listen(subreddit='freebots') \
+    .listen(subreddit=os.environ.get("SUBREDDIT")) \
     .reply(triggers=['imp', 'dwarf', 'halfman', 'half-man', 'half man', 'tyrion bot', 'tyrion-bot'],
            replies=quotes.split(sep='\n') + LambdaQuotes.all()) \
     .run()
